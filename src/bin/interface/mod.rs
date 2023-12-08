fd1d28b997a58c466ae18f2156fd9c5e395d80d6
Benjamin Haase <benjaminthaase@protonmail.com>
2023-12-08 10:17:43 -0800

    Adds instrument proc-macro and events to Interface struct

--- src/bin/interface/mod.rs
+++ src/bin/interface/mod.rs
@@ -1,11 +1,10 @@
-//! Provides the necessary components for creating a smoothing CLI for clients interacting
+//! Provides the necessary components for creating a CLI for clients interacting
 //! with the server.
 //!
 
-// use std::io::BufRead;
 use std::marker::{Unpin, Send};
 use async_std::io::{Write, WriteExt, ReadExt, prelude::{BufRead, BufReadExt}, Read};
-use async_std::task;
+use tracing::{instrument, event, Level};
 use async_jira_cli::response::prelude::*;
 use async_jira_cli::utils::prelude::*;
 use crate::UserError;
@@ -14,9 +13,6 @@ mod frame;
 mod pages;
 
 use pages::prelude::*;
-
-// use frame::prelude::*;
-// use pages::prelude::*;
 
 pub mod prelude {
     pub use super::*;
@@ -39,10 +35,6 @@ where
     client_input: R,
 }
 
-// TODO: 1) move response conditional blocks into separate functions
-// TODO: 2) Refactor design such that the interface has ownership connection to the stream
-// TODO: 3) implement a parse_option method for taking a user selected option input from stdin
-//          and parsing it correctly i.e. implementing the logic needed for the users given selection
 impl<R, T> Interface<R, T>
 where
     R: BufRead + Send + Unpin,
@@ -57,6 +49,8 @@ where
         }
     }
 
+    /// Starts the CLI application and executes the main client loop.
+    #[instrument(ret, err, skip(self))]
     pub async fn run(&mut self) -> Result<(), UserError> {
         let mut tag_buf = [0u8; 18];
         'outer: loop {
@@ -68,6 +62,7 @@ where
 
             'inner: loop {
                 let mut user_option = String::new();
+
                 self.client_input.read_line(&mut user_option)
                     .await
                     .map_err(|_| UserError::ParseRequestOption)?;
@@ -79,6 +74,7 @@ where
                     Ok(action) => {
                         match action {
                             Action::Quit =>  {
+                                event!(Level::INFO, "client terminated program");
                                 println!("program terminated");
                                 break 'outer;
                             }
@@ -106,9 +102,8 @@ where
                         }
                     }
                     Err(e) => {
+                        event!(Level::ERROR, error = ?e, "error when parsing clients request from standard input");
                         println!("{e}");
-                        // TODO: log error as well
-                        eprintln!("{e}");
                         println!();
                         // We always want to give the client another attempt when ever an error occurs
                         // from parsing a request
@@ -126,6 +121,7 @@ where
     /// Attempt to read a response from the stream. If response read correctly, then parse frames
     /// from the read data and create a new page and ad page to page_stack, or display a message and a current page depending
     /// on the logic.
+    #[instrument(ret, err, skip(self))]
     pub async fn parse_response(&mut self, tag_buf: &[u8; 18]) -> Result<(), UserError> {
         let (type_and_flag, epic_id, story_id, data_len) = Response::decode(tag_buf);
 
@@ -134,8 +130,7 @@ where
             self.parse_successful_connection_response(data_len).await?;
 
         } else if type_and_flag & 2 != 0 {
-            // This is a suspicious case, it should not happen so return an internal server error to the client
-            // TODO: Add logging for this event as well
+            event!(Level::WARN, "client already exists");
             return Err(UserError::InternalServerError);
         } else if type_and_flag & 4 != 0 {
             self.parse_add_epic_response(epic_id, data_len).await?;
@@ -168,6 +163,7 @@ where
             self.parse_update_story_status_response(story_id, data_len).await?;
 
         } else if type_and_flag & 4096 != 0 {
+            event!(Level::ERROR, "server could not parse clients request");
             println!("request not parsed, please try again");
             println!();
 
@@ -175,12 +171,15 @@ where
 
             self.page_stack[self.page_stack.len() - 1].print_page();
         } else {
+            event!(Level::ERROR, "unable to parse response from server");
             return Err(UserError::ParseResponseError(String::from("unable to parse response from server correctly")));
         }
 
         Ok(())
     }
 
+    /// Helper method for parsing the user's input from the proper page on the top of the page stack.
+    #[instrument(ret, err, skip(self))]
     async fn parse_request_option(&mut self, option: &str) -> Result<Action, UserError> {
         assert!(!self.page_stack.is_empty(), "pages should have been loaded prior to calling `self.parse_request_option()`");
         let top_pos = self.page_stack.len() - 1;
@@ -189,6 +188,8 @@ where
         top_page.parse_request(option, &mut self.client_input).await
     }
 
+    /// Helper method for parsing a successful connection response received from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_successful_connection_response(&mut self, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -196,7 +197,7 @@ where
             .map_err(|_|
                 UserError::ParseResponseError(format!("unable to parse response from server correctly"))
             )?;
-
+        event!(Level::INFO, "successfully connected to database");
         println!("connected to database successfully");
         println!();
 
@@ -211,6 +212,7 @@ where
         Ok(())
     }
 
+    #[instrument(ret, err, skip(self))]
     async fn parse_add_epic_response(&mut self, epic_id: u32, data_len: u64) -> Result<(), UserError> {
         // Read updated homepage data
         let mut data = vec![0; data_len as usize];
@@ -222,7 +224,7 @@ where
 
         // Attempt to create the new homepage
         let homepage = Box::new(HomePage::try_create(data)?);
-
+        event!(Level::INFO, epic_id, "epic added successfully, epic id: {}", epic_id);
         // display success message to user
         println!("epic added successfully, epic id: {}", epic_id);
         println!();
@@ -234,6 +236,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a delete epic response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_delete_epic_response(&mut self, epic_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -243,7 +247,7 @@ where
             )?;
         // Attempt to create homepage
         let homepage = Box::new(HomePage::try_create(data)?);
-
+        event!(Level::INFO, epic_id, "epic with id: {} was deleted successfully", epic_id);
         println!("epic with id: {} was deleted successfully", epic_id);
         println!();
 
@@ -254,6 +258,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a get epic response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_get_epic_response(&mut self, epic_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -261,6 +267,7 @@ where
             .map_err(|_| UserError::ParseResponseError(String::from("unable to parse response from server correctly")))?;
         let epic_detail_page = Box::new(EpicDetailPage::try_create(data)?);
 
+        event!(Level::INFO, epic_id, "received epic: {} successfully", epic_id);
         println!("get epic with id: {} successful", epic_id);
         println!();
 
@@ -270,6 +277,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a epic status update response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_update_epic_status_response(&mut self, epic_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -278,6 +287,7 @@ where
 
         let epic_detail_page = Box::new(EpicDetailPage::try_create(data)?);
 
+        event!(Level::INFO, epic_id, "status of epic {} was updated successfully", epic_id);
         println!("updated status of epic with id: {} successful", epic_id);
         println!();
 
@@ -289,6 +299,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing epic does not exist response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_epic_does_not_exist_response(&mut self, epic_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -297,6 +309,7 @@ where
 
         let homepage = Box::new(HomePage::try_create(data)?);
 
+        event!(Level::INFO, epic_id, "epic with id: {} does not exist", epic_id);
         println!("epic with id: {} does not exist", epic_id);
         println!();
 
@@ -307,6 +320,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a get story response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_get_story_response(&mut self, story_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -315,6 +330,7 @@ where
 
         let story_detail_page = Box::new(StoryDetailPage::try_create(data)?);
 
+        event!(Level::INFO, story_id, "received story: {} successfully", story_id);
         println!("get story with id: {} successful", story_id);
         println!();
 
@@ -324,6 +340,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a story does not exist response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_story_does_not_exist_response(&mut self, story_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -332,6 +350,7 @@ where
 
         let epic_detail_page = Box::new(EpicDetailPage::try_create(data)?);
 
+        event!(Level::INFO, story_id, "story with id: {} does not exist", story_id);
         println!("story with id: {} does not exist", story_id);
         println!();
 
@@ -346,6 +365,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing an add story response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_add_story_response(&mut self, story_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -354,6 +375,7 @@ where
 
         let epic_detail_page = Box::new(EpicDetailPage::try_create(data)?);
 
+        event!(Level::INFO, story_id, "story with id: {} was added successfully", story_id);
         println!("story with id: {} was added successfully", story_id);
         println!();
 
@@ -365,6 +387,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a delete story response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_delete_story_response(&mut self, story_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -373,6 +397,7 @@ where
 
         let epic_detail_page = Box::new(EpicDetailPage::try_create(data)?);
 
+        event!(Level::INFO, story_id, "deleted story {} successfully", story_id);
         println!("story with id: {} was deleted successfully", story_id);
         println!();
 
@@ -387,6 +412,8 @@ where
         Ok(())
     }
 
+    /// Helper method for parsing a story status update response from the server.
+    #[instrument(ret, err, skip(self))]
     async fn parse_update_story_status_response(&mut self, story_id: u32, data_len: u64) -> Result<(), UserError> {
         let mut data = vec![0; data_len as usize];
         self.connection_stream.read_exact(data.as_mut_slice())
@@ -395,6 +422,7 @@ where
 
         let story_detail_page = Box::new(StoryDetailPage::try_create(data)?);
 
+        event!(Level::INFO, story_id, "updated status of story with id: {} was successful", story_id);
         println!("updated status of story with id: {} was successful", story_id);
         println!();
 
